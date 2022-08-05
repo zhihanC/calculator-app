b2f53667dac38f5ff648b3aac4f6fee7230595c6
zhihanC <67808576+zhihanC@users.noreply.github.com>
2022-08-04 22:14:16 -0400

    Added listeners to buttons, vars for calculation, design tweaks

---
+++ scripts.js
@@ -0,0 +1,123 @@
+let display = document.getElementById('display');
+
+let num1;
+let num2;
+let result;
+
+const clear = document.getElementById('clear');
+
+clear.addEventListener('click', () => {
+  console.log('clear button was pressed');
+});
+
+const invert = document.getElementById('invert');
+
+invert.addEventListener('click', () => {
+  console.log('invert button was pressed');
+});
+
+const percent = document.getElementById('percent');
+
+percent.addEventListener('click', () => {
+  console.log('percent button was pressed');
+});
+
+//operations begin here
+
+const divide = document.getElementById('divide');
+
+divide.addEventListener('click', () => {
+  console.log('divide button was pressed');
+});
+
+const multiply = document.getElementById('multiply');
+
+multiply.addEventListener('click', () => {
+  console.log('multiply button was pressed');
+});
+
+const subtract = document.getElementById('subtract');
+
+subtract.addEventListener('click', () => {
+  console.log('subtract button was pressed');
+});
+
+const add = document.getElementById('add');
+
+add.addEventListener('click', () => {
+  console.log('add button was pressed');
+});
+
+const decimal = document.getElementById('decimal');
+
+decimal.addEventListener('click', () => {
+  console.log('decimal button was pressed');
+});
+
+const equal = document.getElementById('equal');
+
+equal.addEventListener('click', () => {
+  console.log('equal button was pressed');
+});
+
+// numbers begin here
+
+const zero = document.getElementById('zero');
+
+zero.addEventListener('click', () => {
+  console.log('zero button was pressed');
+});
+
+const one = document.getElementById('one');
+
+one.addEventListener('click', () => {
+  console.log('one button was pressed');
+});
+
+const two = document.getElementById('two');
+
+two.addEventListener('click', () => {
+  console.log('two button was pressed');
+});
+
+const three = document.getElementById('three');
+
+three.addEventListener('click', () => {
+  console.log('three button was pressed');
+});
+
+const four = document.getElementById('four');
+
+four.addEventListener('click', () => {
+  console.log('four button was pressed');
+});
+
+const five = document.getElementById('five');
+
+five.addEventListener('click', () => {
+  console.log('five button was pressed');
+});
+
+const six = document.getElementById('six');
+
+six.addEventListener('click', () => {
+  console.log('six button was pressed');
+});
+
+const seven = document.getElementById('seven');
+
+seven.addEventListener('click', () => {
+  console.log('seven button was pressed');
+});
+
+const eight = document.getElementById('eight');
+
+eight.addEventListener('click', () => {
+  console.log('eight button was pressed');
+});
+
+const nine = document.getElementById('nine');
+
+nine.addEventListener('click', () => {
+  console.log('nine button was pressed');
+});
