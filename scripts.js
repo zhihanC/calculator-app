015848d4c328a3d191d39287e44d76e8faeaba01
zhihanC <67808576+zhihanC@users.noreply.github.com>
2022-08-07 18:47:25 -0400

    Added logic for operation buttons, documented one known bug

--- scripts.js
+++ scripts.js
@@ -1,16 +1,20 @@
 let display = document.getElementById('display');
 
-let num1 = 0; // keeps track of the first num
-let num2 = 0; // keeps track of the second num
-let numSwitch = false; // switches which num is being displayed
-let dpt = false; // if true, increments num by 10, else increments by dptCounter
-let dptCounter = 0.1; // keeps track of decimal increments
+var num1 = 0; // keeps track of the first num
+var num2 = 0; // keeps track of the second num
+//var result = 0; // keeps track of final answer
+var numSwitch = false; // switches which num is being displayed
+var dpt = false; // if true, increments num by 10, else increments by dptCounter
+var dptCounter = 0.1; // keeps track of decimal increments
+var operation = 0; // 1: add / 2: subtract / 3: multiply / 4: divide
+var newEQ = false; // allows for new EQs in addition to chaining EQs
 
 const clear = document.getElementById('clear');
 
 clear.addEventListener('click', () => {
   display.innerHTML = '0';
   num1 = 0;
+  num2 = 0;
 });
 
 const invert = document.getElementById('invert');
@@ -34,7 +38,21 @@ decimal.addEventListener('click', () => {
 const equal = document.getElementById('equal');
 
 equal.addEventListener('click', () => {
-  console.log('equal button was pressed');
+  if (operation == 1) {
+    num1 = num1 + num2;
+  } else if (operation == 2) {
+    num1 = num1 - num2;
+  } else if (operation == 3) {
+    num1 = num1 * num2;
+  } else {
+    num1 = num1 / num2;
+  }
+  display.innerHTML = num1;
+  num2 = 0;
+  numSwitch = false;
+  newEQ = true;
+  console.log('num1: ', num1);
+  console.log('num2: ', num2);
 });
 
 //operations begin here
@@ -42,25 +60,59 @@ equal.addEventListener('click', () => {
 const divide = document.getElementById('divide');
 
 divide.addEventListener('click', () => {
-  console.log('divide button was pressed');
+  numSwitch = true;
+  operation = 4;
+  console.log('num1: ', num1);
+  console.log('num2: ', num2);
+
+  if (num2 == 0) {
+    display.innerHTML = num1;
+  } else {
+    num1 = num1 / num2;
+    num2 = 0;
+    display.innerHTML = num1;
+  }
 });
 
 const multiply = document.getElementById('multiply');
 
 multiply.addEventListener('click', () => {
-  console.log('multiply button was pressed');
+  numSwitch = true;
+  operation = 3;
+  console.log('num1: ', num1);
+  console.log('num2: ', num2);
+
+  if (num2 == 0) {
+    display.innerHTML = num1;
+  } else {
+    num1 = num1 * num2;
+    num2 = 0;
+    display.innerHTML = num1;
+  }
 });
 
 const subtract = document.getElementById('subtract');
 
 subtract.addEventListener('click', () => {
-  console.log('subtract button was pressed');
+  numSwitch = true;
+  operation = 2;
+  console.log('num1: ', num1);
+  console.log('num2: ', num2);
+  num1 = num1 - num2;
+  num2 = 0;
+  display.innerHTML = num1;
 });
 
 const add = document.getElementById('add');
 
 add.addEventListener('click', () => {
-  console.log('add button was pressed');
+  numSwitch = true;
+  operation = 1;
+  console.log('num1: ', num1);
+  console.log('num2: ', num2);
+  num1 = num1 + num2;
+  num2 = 0;
+  display.innerHTML = num1;
 });
 
 // numbers begin here
@@ -68,87 +120,103 @@ add.addEventListener('click', () => {
 const zero = document.getElementById('zero');
 
 zero.addEventListener('click', () => {
-  display.innerHTML = '0';
+  numInput(0);
 });
 
 const one = document.getElementById('one');
 
 one.addEventListener('click', () => {
-  if (numSwitch) {
-    if (num2 == 0) {
-      num2 = 1;
-    } else if (dpt) {
-      num2 = num2 + dptCounter * 1;
-      dptCounter = dptCounter / 10;
-    } else {
-      num2 = num2 * 10 + 1;
-    }
-    display.innerHTML = num2;
-  } else {
-    if (num1 == 0) {
-      num1 = 1;
-    } else if (dpt) {
-      num1 = num1 + dptCounter * 1;
-      dptCounter = dptCounter / 10;
-    } else {
-      num1 = num1 * 10 + 1;
-    }
-    display.innerHTML = num1;
-  }
+  numInput(1);
 });
 
 const two = document.getElementById('two');
 
 two.addEventListener('click', () => {
-  console.log('two button was pressed');
-  display.innerHTML = '2';
+  numInput(2);
 });
 
 const three = document.getElementById('three');
 
 three.addEventListener('click', () => {
-  console.log('three button was pressed');
-  display.innerHTML = '3';
+  numInput(3);
 });
 
 const four = document.getElementById('four');
 
 four.addEventListener('click', () => {
-  console.log('four button was pressed');
-  display.innerHTML = '4';
+  numInput(4);
 });
 
 const five = document.getElementById('five');
 
 five.addEventListener('click', () => {
-  console.log('five button was pressed');
-  display.innerHTML = '5';
+  numInput(5);
 });
 
 const six = document.getElementById('six');
 
 six.addEventListener('click', () => {
-  console.log('six button was pressed');
-  display.innerHTML = '6';
+  numInput(6);
 });
 
 const seven = document.getElementById('seven');
 
 seven.addEventListener('click', () => {
-  console.log('seven button was pressed');
-  display.innerHTML = '7';
+  numInput(7);
 });
 
 const eight = document.getElementById('eight');
 
 eight.addEventListener('click', () => {
-  console.log('eight button was pressed');
-  display.innerHTML = '8';
+  numInput(8);
 });
 
 const nine = document.getElementById('nine');
 
 nine.addEventListener('click', () => {
-  console.log('nine button was pressed');
-  display.innerHTML = '9';
+  numInput(9);
 });
+
+// Purpose: function for inputting numbers
+// Param: number is the specific int tied to the button
+// Bug: floating decimals?
+function numInput(number) {
+  // if (newEQ) {
+  //   num1 = number;
+  //   display.innerHTML = num1;
+  // } else {
+
+  // }
+  // newEQ = false;
+  if (numSwitch) {
+    if (num2 == 0) {
+      num2 = number;
+    } else if (dpt) {
+      num2 = num2 + dptCounter * number;
+      dptCounter = dptCounter / 10;
+    } else {
+      num2 = num2 * 10 + number;
+    }
+    display.innerHTML = num2;
+  } else {
+    if (num1 == 0) {
+      num1 = number;
+    } else if (dpt) {
+      num1 = num1 + dptCounter * number;
+      dptCounter = dptCounter / 10;
+    } else {
+      num1 = num1 * 10 + number;
+    }
+    display.innerHTML = num1;
+  }
+}
+
+// Bug Tracker
+// After solving an equation with the equal sign, upon pressing another number button, instead of
+// replacing the previous answer with a new number, it is tacking on the new number onto the end of
+// the previous answer. Line 207 - 209 is responsible for this behavior as this is what we usually
+// want to happen
+// Possible solutions:
+// 1. newEQ var where it turns true after solving an equation with equal sign, then
+// the function knows to begin a new equation (there are additional problems tho)
+// 2. results var that we can keep separate from the num1 var? I like this one so far
